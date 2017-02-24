12d5d4890726e2a694e9572244f2aba9425509c0
mkChung924 <wjdaudrb7@yahoo.com>
2017-02-24 09:43:14 +0900

    Update GuGuDanTest.java

--- Java_Code/GuGuDanTest.java
+++ Java_Code/GuGuDanTest.java
@@ -7,16 +7,27 @@ class GuGuDanTest
 	public static void main(String args[]) throws IOException
 	{
 
-		for(int i = 3; i < 7; i++){
+		for(int i = 1; i < 3; i++){
 			System.out.println("< " + i + "단 >");
 			for(int j = 1 ; j < 10; j++){
-				System.out.println(i + " x " + j + " = " + (i*j));
+				System.out.println(i + "x" + j + "= " + (i*j));
 
 			}
 			System.out.println();
 		}
 
-		System.out.println("######################################################");
+		for(int i = 1; i < 10; i++){
+			for(int j = 1; j < 10; j++){
+				int mult = j*i;
+				String zero = "";
+				if(mult < 10) zero = " ";
+
+				System.out.print(j + "x" + i + "= " + zero + mult + "   ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("\n######################################################");
 		System.out.println("################구구단 출력 프로그램##################");
 		System.out.println("######################################################");
 		System.out.print("원하는 단을 입력하시오.(0: 모든 구구단(1~9)): ");
@@ -35,9 +46,10 @@ class GuGuDanTest
 
 	static void gugudan(int b)
 	{
+		System.out.println("< " + b + "단 >");
 		for(int i = 1; i < 10 ; i++)
 		{
-			System.out.println(b + " x " + i + " = " + (b*i));
+			System.out.println(b + "x" + i + "= " + (b*i));
 		}
 		System.out.println(">>>>>>>>>> "+ b + "단 출력 완료! <<<<<<<<<<");
 	}
@@ -48,7 +60,7 @@ class GuGuDanTest
 		{
 			System.out.println(i + "단");
 			for(int j = 1; j < 10 ; j++){
-				System.out.println(i + " x " + j + " = " + (i*j));
+				System.out.println(i + "x" + j + "= " + (i*j));
 			}
 			System.out.println();
 		}
