920aa3971942b8ed116a8560af9b9b302337ab20
mkChung924 <wjdaudrb7@yahoo.com>
2017-02-24 10:28:41 +0900

    Update GuGuDanTest.java

--- Java_Code/GuGuDanTest.java
+++ Java_Code/GuGuDanTest.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-//import java.io.*; <-- not recommanded
+//import java.io.*; <-- not recommended
 class GuGuDanTest
 {
 	public static void main(String args[]) throws IOException
