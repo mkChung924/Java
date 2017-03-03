c07b82570e6339405694e71360540fd9c80eb4a0
myungkyuchung <myungkyuchung@192.168.0.127>
2017-03-03 18:59:24 +0900

    03/03 updates

---
+++ JavaApp/src/com/kosta/j0303/NameMenu.java
@@ -0,0 +1,114 @@
+package com.kosta.j0303;
+
+public class NameMenu {
+	
+	String names[];
+	
+	public NameMenu() {
+		
+		names = new String[5];
+		//System.out.println("haha");
+	}
+	
+	public void insert(String addName){ //이름 추가
+		
+		
+		for(int i = 0; i < names.length; i++){
+			
+			if(names[i] != null){
+				if(names[i].equals(addName)){
+				System.out.println("동일한 이름이 존재합니다.");
+				return;
+				}
+			} else if(names[i] == null){
+				names[i] = addName;
+				break;
+			}			
+		}
+		System.out.println("저장완료!");
+		
+		
+
+	}
+	
+	public void select(){ // 이름 저장소를 화면 출력; 모든이름 출력
+		
+		long start = System.currentTimeMillis();
+		
+		if(names[0] != null){
+			System.out.println("#이름목록");
+			for(int i = 0; i < names.length; i++){
+				if(names[i] != null){
+					System.out.println("["+i+"] "+ names[i]);
+				} 
+			}
+			
+			long end = System.currentTimeMillis();
+			System.out.println( "*실행 시간 : " + ( end - start )/1000.0 + "s");
+			
+		} else {
+			
+				System.out.println("저장된 이름이 없습니다. 먼저 추가해 주세요.");
+				return;
+			} 
+		
+		
+		}
+		
+	public boolean listCheck(){
+		if(names[0] != null){
+			return true;
+		} else {
+			System.out.println("이름 목록이 비었습니다. 먼저 추가해 주세요.");
+			return false;
+		}
+		
+	}
+	
+	public void update(String oldName, String newName){ // 이름 변경
+		
+		if(names[0] != null){
+			for(int i = 0; i < names.length; i++){
+				if(names[i].equals(oldName)){
+					names[i] = newName;
+					break;
+				}
+			}
+				System.out.println("변경 완료!");
+		} else {
+			System.out.println("이름이 존재하지 않습니다.");
+		}
+		
+		
+	}
+	
+	public void delete(String delName){ // 이름 삭제
+		
+		long start = System.currentTimeMillis();
+		
+		int i;
+		for(i = 0; i < names.length; i++){
+			if(names[i] == null){
+				System.out.println("이름을 찾을 수 없습니다.");
+				return;
+			} else {
+			if(names[i].equals(delName)){
+				names[i] = null;
+				break;
+				}
+			}
+		}
+
+		for(int j = i; j < names.length; j++){
+			if(j < names.length -1 ) {
+				names[j] = names[j+1];
+			}
+		}
+		
+		System.out.println("삭제 완료!");
+		long end = System.currentTimeMillis();
+		System.out.println( "*실행 시간 : " + ( end - start )/1000.0 + "s");
+		
+	}
+
+}
---
+++ JavaApp/src/com/kosta/j0303/NameMenuTest.java
@@ -0,0 +1,94 @@
+package com.kosta.j0303;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//<클래스 작성>
+//1. NameMenu.java(이름 추가, 수정, 삭제, 검색 기능 메소드를 위치)
+//	- 멤버: 배열객체 String names[];
+//	- 추가(add, append, plus, insert)
+//	- 검색(search, find, select)
+//	- 수정(modify, change, update, replace, rename)
+//	- 삭제(delete, remove, clear, truncate, erase)
+
+//2. NameMenuTest.java(main()메소드 포함, 반복되는 메뉴 출력)
+public class NameMenuTest {
+	
+	
+	public NameMenuTest() { //기본 생성
+	}
+	
+
+	public static void main(String args[]) throws IOException{
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num;
+		
+		NameMenu nm = new NameMenu();
+		
+		do{
+
+		System.out.println("<이름메뉴>");
+		System.out.println("1.추가 2.검색 3.수정 4.삭제 5.종료");
+		System.out.print("번호입력 ==> ");
+		num = Integer.parseInt(br.readLine()); // "1", "2", "3", "4", "5"
+		
+		switch(num){
+		case 1: 
+			String name;
+			System.out.print("이름 입력: ");
+			name = br.readLine();
+			nm.insert(name);
+			
+			break;
+			
+		case 2:
+			nm.select();
+			
+			break;
+		
+		case 3:
+			String oldName;
+			String newName;
+			if(nm.listCheck()){
+			System.out.print("기존 이름: ");
+			oldName = br.readLine();
+			System.out.print("새 이름: ");
+			newName = br.readLine();
+			
+			nm.update(oldName, newName);
+			}
+			break;
+			
+			
+		case 4:
+					
+			String delName;
+			if(nm.listCheck()){
+			System.out.print("이름 입력: ");
+			delName = br.readLine();
+			nm.delete(delName);
+			}
+			break;
+			
+		case 5:
+			
+			break;
+			
+			
+		}
+			
+			
+			System.out.println();
+			
+		} while(num != 5); //입력 받는 번호가 5번이 아닐때 계속 loop
+		
+		System.out.println("-- END --");
+		
+
+	}//main
+
+} //NameMenuTest 클래스
+
